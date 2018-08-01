8e6cb1a97cd0b445da197d8ebaa9fb51ff799dfd
Zi Jian CHUA <chuazj@ncs.com.sg>
2018-08-01 22:01:12 +0800

    Updated default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,5 +12,9 @@ default['audit']['profiles'] =
         name: 'CIS CentOS Linux 7 Benchmark Level 1',
         compliance: 'admin/cis-centos7-level1',
       },
+      {
+        name: 'CIS CentOS Linux 7 Benchmark Level 2',
+        compliance: 'admin/cis-centos7-level2',
+      },
     ]
 end
